29cee746015cc90b666f3156b5dc57cac88ee2e5
NadisaFebriadha <nadisafebriadhama@gmail.com>
2023-08-02 17:26:26 +0800

    belajar variabel

---
+++ Variabel-js.js
@@ -0,0 +1,35 @@
+var x = 20;
+var y = 100;
+
+console.log ("x nilai lama",x);
+console.log (y);
+
+x = 100;
+y = 20;
+
+console.log ("x nilai baru",x)
+console.log ("y nilai baru",y)
+
+x = 20;
+y = 100;
+z = x + y;
+
+console.log ("x + y",z)
+
+let angka1 = 2021;
+let angka2 = 20.22;
+let angka3 = 20.22;
+
+console.log (angka1 + angka2 + angka3);
+
+var nama1 = "Belajar variabel tipe string"
+var nama2 = "Belajar variabel tipe number"
+
+console.log (nama1,nama2)
+
+
+
+
+
+
+
